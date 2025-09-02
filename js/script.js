d66cc926c640a9bf1f6951820288f38518b1c456
= <cavaleiromiau@gmail.com>
2025-09-01 23:22:16 -0300

    new update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
 function setActiveLink() {
   const currentPath = window.location.pathname;
-
   const navLinks = document.querySelectorAll('.nav-menu a, .mobile-nav-panel a');
-
   navLinks.forEach(link => {
     const linkPath = new URL(link.href).pathname;
-
-
     if ((currentPath === '/' || currentPath === '/index.html') && (linkPath === '/' || linkPath === '/index.html')) {
       link.classList.add('active-link');
     } else if (currentPath !== '/' && linkPath === currentPath) {
@@ -14,7 +10,6 @@ function setActiveLink() {
     }
   });
 }
-
 
 class MobileNavbar {
   constructor(mobileMenu, navPanel, navLinks, body) {
@@ -24,36 +19,54 @@ class MobileNavbar {
     this.body = document.querySelector(body);
     this.activeClass = "active";
     this.noScrollClass = "no-scroll";
+    this.scrollPosition = 0;
 
     this.handleClick = this.handleClick.bind(this);
     this.handleLinkClick = this.handleLinkClick.bind(this);
   }
 
+  lockScroll() {
+    this.scrollPosition = window.scrollY;
+    this.body.style.top = `-${this.scrollPosition}px`;
+    document.documentElement.classList.add(this.noScrollClass);
+    this.body.classList.add(this.noScrollClass);
+  }
+
+  unlockScroll() {
+    document.documentElement.classList.remove(this.noScrollClass);
+    this.body.classList.remove(this.noScrollClass);
+    this.body.style.top = '';
+    window.scrollTo(0, this.scrollPosition);
+  }
+
   handleClick() {
+    const isActive = this.navPanel.classList.contains(this.activeClass);
+    if (isActive) {
+      this.unlockScroll();
+    } else {
+      this.lockScroll();
+    }
     this.navPanel.classList.toggle(this.activeClass);
     this.mobileMenu.classList.toggle(this.activeClass);
-    this.body.classList.toggle(this.noScrollClass);
   }
 
   closeMenu() {
-    this.navPanel.classList.remove(this.activeClass);
-    this.mobileMenu.classList.remove(this.activeClass);
-    this.body.classList.remove(this.noScrollClass);
+    if (this.navPanel.classList.contains(this.activeClass)) {
+      this.unlockScroll();
+      this.navPanel.classList.remove(this.activeClass);
+      this.mobileMenu.classList.remove(this.activeClass);
+    }
   }
 
   handleLinkClick(event) {
     const link = event.currentTarget.querySelector('a');
     if (!link) return;
-
     const href = link.getAttribute('href');
-
     if (href.startsWith('#')) {
       event.preventDefault();
-
       const targetElement = document.querySelector(href);
       if (targetElement) {
         this.closeMenu();
-
         setTimeout(() => {
           targetElement.scrollIntoView({ behavior: 'smooth' });
         }, 400);
